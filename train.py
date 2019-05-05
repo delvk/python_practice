79ebc826aef0848f32176606135427aebb025e3f
khuong <nbkhuong97@gmail.com>
2019-05-05 17:13:55 +0700

    adding little more

--- train.py
+++ train.py
@@ -5,21 +5,39 @@ This script suppose to do this things
 3. Training, save model with best result each epoch
 """
 # import libary
-import tensorflow as tf 
+import tensorflow as tf
 import src.loader as loader
 import src.layer as layer
 import utils as utils
-def load_data(data_root):
-    val_image_list, val_gt_list = utils.get_data_list(data_root, mode='valid')
-    images = 0
-    density_maps = 0
+from skimage.color import rgb2gray
+from skimage.data import imread
+import numpy as np
+import pandas as pd
+
+
+def load_data(data_root, mode):
+    image_list = []
+    gt_list = []
+    if mode == 'train':
+        image_list, gt_list = utils.get_data_list(data_root, mode='train')
+    elif mode == 'valid':
+        image_list, gt_list = utils.get_data_list(data_root, mode='valid')
+    else:
+        image_list, gt_list = utils.get_data_list(data_root, mode='test')
+
+    # convert images and index to array
+    for image_index in range(len(image_list)):
+        # load image and ground truth
+        images = rgb2gray(np.asarray(imread(image_list[image_index]), dtype=np.float32))
+        density_maps = np.asarray(pd.read_csv(
+            gt_list[image_index], header=None), dtype=np.float32)
     return images, density_maps
 
 
-
 def main(args):
-    load_data(args.data_root)
-    print('hello world')
+    images, gt = load_data(args.data_root, mode='valid')
+    print(images.shape)
+    print(gt.shape)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ def get_data_list(root_path, mode='valid'):
     gt_list = []
     for filepath in image_list:
         file_id = get_file_id(filepath)
-        gt_file_path = join(ground_truth_path, 'GT_'+ file_id + '.mat')
+        gt_file_path = join(ground_truth_path, 'ground_truth'+ file_id + '.csv')
         gt_list.append(gt_file_path)
     
     xy = list(zip(image_list, gt_list))
@@ -41,3 +41,19 @@ def get_data_list(root_path, mode='valid'):
     s_image_list, s_gt_list = zip(*xy)
 
     return s_image_list, s_gt_list
+
+# def create_session(log_dir, session_id):
+#     """
+#     Module to create a session folder. It will create a folder with a proper session
+#     id and return the session path.
+#     :param log_dir: root log directory
+#     :param session_id: ID of the session
+#     :return: path of the session id folder
+#     """
+#     folder_path = os.path.join(log_dir, 'session:'+str(session_id))
+#     if os.path.exists(folder_path):
+#         print ('Session already taken. Please select a different session id.')
+#         sys.exit()
+#     else:
+#         os.makedirs(folder_path)
+#     return folder_path
